440e6ab9e08eeb74a4553d0e7025ca92eb4ec1f9
Roman Belov <rnbelov96@gmail.com>
2020-04-08 18:14:43 +0300

    csrf отключен

--- app.js
+++ app.js
@@ -41,17 +41,17 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(csrf());
-app.use(function(req, res, next) {
-  let token = req.csrfToken();
-  res.cookie('X-CSRF-TOKEN', token, {
-    sameSite: 'none',
-    secure: true,
-    domain: 'roh7771.github.io'
-  });
-  res.locals.csrfToken = token;
-  next();
-});
+// app.use(csrf());
+// app.use(function(req, res, next) {
+//   let token = req.csrfToken();
+//   res.cookie('X-CSRF-TOKEN', token, {
+//     sameSite: 'none',
+//     secure: true,
+//     domain: 'roh7771.github.io'
+//   });
+//   res.locals.csrfToken = token;
+//   next();
+// });
 
 // 3) ROUTES
 app.use("/api/v1/substances", substanceRouter);
