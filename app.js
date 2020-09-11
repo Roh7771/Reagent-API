057c51cd097f06ee86b599d1d6d3345ec271298f
Roman Belov <belov.rom-77@yandex.ru>
2020-09-11 08:34:41 +0300

    Обновляет CORS конфигурацию

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 const cookieParser = require('cookie-parser');
 
 const corsOptions = {
-  origin: "*",
+  origin: [/^https:\/\/roh7771\.github\.io/, "http://localhost:9000"],
   credentials: true,
 };
 
